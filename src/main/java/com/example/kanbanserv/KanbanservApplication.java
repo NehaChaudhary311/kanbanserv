318db7b5a39e7aafb88a5024c9fb5357e4554115
nechaudhary <nechaudhary@paypal.com>
2022-06-15 21:48:09 +0530

    init

---
+++ src/main/java/com/example/kanbanserv/KanbanservApplication.java
@@ -0,0 +1,13 @@
+package com.example.kanbanserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kanbanserv/KanbanservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kanbanserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

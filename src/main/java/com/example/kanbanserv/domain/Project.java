6aedae277bd57a3aceb55598cd3cd3056e7be336
nechaudhary <nechaudhary@paypal.com>
2022-06-15 23:14:01 +0530

    feat(app):added entities for project

--- src/main/java/com/example/kanbanserv/domain/Project.java
+++ src/main/java/com/example/kanbanserv/domain/Project.java
@@ -1,4 +1,35 @@
 package com.example.kanbanserv.domain;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectIdentifier;
+    private String projectName;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public Project(){
+
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
 }

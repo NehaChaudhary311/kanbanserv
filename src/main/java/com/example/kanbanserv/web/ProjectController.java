8f34da908dedd6f1c75766e347175dbaa437b7fa
nechaudhary <nechaudhary@paypal.com>
2022-06-22 23:57:40 +0530

    feat(app):Errors and Error response

--- src/main/java/com/example/kanbanserv/web/ProjectController.java
+++ src/main/java/com/example/kanbanserv/web/ProjectController.java
@@ -1,17 +1,24 @@
 package com.example.kanbanserv.web;
 
 import com.example.kanbanserv.domain.Project;
+import com.example.kanbanserv.domain.error.Error;
+import com.example.kanbanserv.domain.error.ErrorDetails;
 import com.example.kanbanserv.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -24,7 +31,19 @@ public class ProjectController {
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, Errors errors){
 
         if(errors.hasErrors()){
-            return new ResponseEntity<String>("Invalid body received", HttpStatus.BAD_REQUEST);
+            List<ErrorDetails> errorList = new ArrayList<>();
+            for(FieldError error: errors.getFieldErrors()){
+                errorList.add(ErrorDetails.builder()
+                                          .field(error.getField())
+                                          .description(error.getDefaultMessage())
+                                          .build());
+            }
+
+            return new ResponseEntity<Error>(Error.builder()
+                                                  .name("INVALID_REQUEST")
+                                                  .message("Invalid Request")
+                                                  .errorDetailsList(errorList)
+                                                  .build(), HttpStatus.BAD_REQUEST);
         }
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
